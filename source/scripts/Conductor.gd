609454a1adf68ac2fed2f774c1ff6d9ac07fc437
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-07-18 11:25:19 -0500

    slight conductor rewrite + titlestate progress

--- source/scripts/Conductor.gd
+++ source/scripts/Conductor.gd
@@ -4,11 +4,6 @@ var bpm:float = 100;
 var crochet:float = ((60 / bpm) * 1000); # beats in ms
 var stepCrochet:float = crochet / 4; # steps in ms
 var songPosition:float;
-var lastSongPos:float;
-
-var offset:float = 0;
-var safeFrames:int = 10;
-var safeZoneOffset:float = (safeFrames / 60) * 1000; # safeFrames in ms
 
 var bpmChangeMap:Array = [];
 
@@ -16,30 +11,12 @@ var curStep:int = 0;
 var curBeat:int = 0;
 var curMeasure:int = 0;
 
-var paused:bool = false;
-
-func pause() -> void:
-	paused = true;
-
-func play() -> void:
-	paused = false;
-
-func reset() -> void:
-	songPosition = 0;
-
 func _process(elapsed:float) -> void:
 	if not paused:
 		songPosition += elapsed * 1000;
 		
 	var oldStep:int = curStep;
 	
-	updateStep();
-	updateBeat();
-	
-	if oldStep != curStep:
-			stepHit();
-
-func updateStep() -> void:
 	var lastChange:Dictionary = {
 		"stepTime": 0,
 		"songTime": 0,
@@ -51,11 +28,21 @@ func updateStep() -> void:
 			lastChange = bpmChangeMap[i];
 		
 	curStep = lastChange["stepTime"] + floor((songPosition - lastChange["songTime"]) / stepCrochet);
-	
-func updateBeat() -> void:
 	curBeat = floor(curStep / 4);
+	
+	if oldStep != curStep:
+		stepHit();
 
-# THE GOODIES
+var paused:bool = false;
+
+func play() -> void:
+	paused = false;
+
+func pause() -> void:
+	paused = true;
+
+func reset() -> void:
+	songPosition = 0;
 
 func stepHit():
 	if curStep % 4 == 0:
