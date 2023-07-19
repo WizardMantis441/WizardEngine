7a775aa38ced54aa59dbef5a317ce51b6dfbc6bb
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-07-19 15:09:00 -0500

    TITLESTATE

--- source/scripts/Conductor.gd
+++ source/scripts/Conductor.gd
@@ -36,11 +36,18 @@ func _process(elapsed:float) -> void:
 	curMeasure = floor(curBeat / 4);
 	
 	if oldStep != curStep:
+		var cur_scene = get_tree().current_scene
 		stepHit.emit(curStep);
+		if cur_scene.has_method("stepHit"):
+			cur_scene.call_deferred("stepHit", curStep)
 		if curStep % 4 == 0:
 			beatHit.emit(curBeat);
+			if cur_scene.has_method("beatHit"):
+				cur_scene.call_deferred("beatHit", curBeat)
 		if curBeat % 4 == 0:
 			measureHit.emit(curMeasure);
+			if cur_scene.has_method("measureHit"):
+				cur_scene.call_deferred("measureHit", curMeasure)
 		# print("curStep ", curStep, ", curBeat ", curBeat, ", curMeasure ", curMeasure);
 
 var paused:bool = false;
--- source/scripts/tools/XML Converter.gd
+++ source/scripts/tools/XML Converter.gd
@@ -115,9 +115,9 @@ func convert_xml() -> void:
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-func _process(_delta:float) -> void:
-	if Input.is_action_just_pressed("ui_cancel") and not fps_box.has_focus():
-		Game.switch_scene("game/menus/MainMenu")
+#func _process(_delta:float) -> void:
+#	if Input.is_action_just_pressed("ui_cancel") and not fps_box.has_focus():
+#		Game.switch_scene("game/menus/MainMenu")
 
 # funny signal shits
 func set_path(new_path:String) -> void:
