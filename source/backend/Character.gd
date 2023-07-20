fd563100b946118a4fcf549c99fe19af937ba37a
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-07-20 01:36:43 -0500

    didnt do that much lol!

--- source/backend/Character.gd
+++ source/backend/Character.gd
@@ -1,13 +1,20 @@
 extends AnimatedSprite2D
 
+var holdTime = 4;
 var lastAnimContext:String = 'NONE'
+
+var anims = ["singLEFT", "singDOWN", "singUP", "singRIGHT"];
+var types = ["left", "down", "up", "right"];
 
 func _ready():
 	pass
 
-
-func _process(delta):
-	pass
+var timer:int = 0;
+func _process(_delta):
+	for i in anims.size():
+		if Input.is_action_just_pressed(types[i]):
+			self.playAnim(anims[i]);
+			timer = 0;
 
 func playAnim(name:String, force:bool = false, context:String = 'NONE'):
 	lastAnimContext = context
@@ -18,5 +25,9 @@ func playAnim(name:String, force:bool = false, context:String = 'NONE'):
 		self.play(name)
 
 func dance():
-	if lastAnimContext != 'LOCK':
+	if lastAnimContext != 'LOCK' and timer >= holdTime:
 		self.playAnim("idle")
+
+func stepHit(curStep):
+	print(timer)
+	timer += 1;
