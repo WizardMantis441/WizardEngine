d693466c2390c745642a87c9ecb202ad225cedd8
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-07-19 21:20:57 -0500

    an idea

---
+++ source/game/Chart.gd
@@ -0,0 +1,9 @@
+class_name Chart extends Node
+
+var bf:Character;
+var dad:Character;
+var gf:Character;
+
+var stage:Stage;
+
+# var notes:Dictionary = Notes or something idk
--- source/game/PlayState.gd
+++ source/game/PlayState.gd
@@ -1,15 +1,5 @@
 extends Node2D
 
-var song = {
-	name: "test",
-	difficulty: "normal"
-}
-
-var song = "test";
-var difficulty = "normal";
-
-var chart = 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
