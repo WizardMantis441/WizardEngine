9337a58160583cf8afb07a33326eac5f193a16d2
InkyBox <85509162+kittstone@users.noreply.github.com>
2023-07-20 11:39:23 -0500

    stupid

--- source/game/Character.gd
+++ source/game/Character.gd
@@ -3,19 +3,10 @@ extends AnimatedSprite2D
 var holdTime = 4;
 var lastAnimContext:String = 'NONE'
 
-var anims = ["singLEFT", "singDOWN", "singUP", "singRIGHT"];
-var types = ["left", "down", "up", "right"];
-
 var timer:int = 0;
 
 func _ready():
 	pass
-
-func _process(_delta):
-	for i in anims.size():
-		if Input.is_action_just_pressed(types[i]):
-			self.playAnim(anims[i], true, 'SING');
-			timer = 0;
 
 func playAnim(name:String, force:bool = false, context:String = 'NONE'):
 	lastAnimContext = context
@@ -26,5 +17,5 @@ func playAnim(name:String, force:bool = false, context:String = 'NONE'):
 		self.play(name)
 
 func dance():
-	if lastAnimContext == 'SING' and timer >= holdTime:
+	if (lastAnimContext == 'SING' or lastAnimContext == 'NONE') and timer >= holdTime:
 		self.playAnim("idle")
--- source/game/PlayState.gd
+++ source/game/PlayState.gd
@@ -2,8 +2,17 @@ extends Node2D
 
 @onready var boyfriend = $Boyfriend
 
+var anims = ["singLEFT", "singDOWN", "singUP", "singRIGHT"];
+var animtypes = ["left", "down", "up", "right"];
+
 func _ready():
-	boyfriend.dance()
+	pass
+	
+func _process(_delta):
+	for i in anims.size():
+		if Input.is_action_just_pressed(animtypes[i]):
+			boyfriend.playAnim(anims[i], true, 'SING');
+			boyfriend.timer = 0;
 
 func beatHit(_curBeat):
 	boyfriend.dance()
