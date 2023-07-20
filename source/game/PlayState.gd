e42062cd8424310c3e77494fd916e3470a3fcfe7
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-07-19 20:11:26 -0500

    help

---
+++ source/game/PlayState.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var song = {
+	name: "test",
+	difficulty: "normal"
+}
+
+var song = "test";
+var difficulty = "normal";
+
+var chart = 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
