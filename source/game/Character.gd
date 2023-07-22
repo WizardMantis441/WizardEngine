bef6b17d4daad9a2a30b1018c4cb8eb016528bd1
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-07-22 15:13:41 -0500

    gotta head to work! lol!

--- source/game/Character.gd
+++ source/game/Character.gd
@@ -5,6 +5,8 @@ extends AnimatedSprite2D
 var json:Dictionary = {}
 
 var holdTime:int = 4;
+var idles:Array = [];
+
 var lastAnimContext:String = 'NONE'
 
 var timer:int = 0;
@@ -17,6 +19,9 @@ func _ready():
 	json = JSON.parse_string(FileAccess.open("res://assets/data/characters/" + curCharacter + ".json", FileAccess.READ).get_as_text())
 	holdTime = json.info.holdTime;
 	globalOffset = Vector2(json.info.x, json.info.y)
+	for i in json.info.idles:
+		idles.push_back(i);
+		print(idles)
 	
 	if self.json.info.flipX:
 		self.flip_h = not self.flip_h
@@ -37,7 +42,7 @@ func playAnim(animname:String, force:bool = false, context:String = 'NONE'):
 
 func dance():
 	if (self.lastAnimContext == 'SING' or self.lastAnimContext == 'NONE' or self.lastAnimContext == 'DANCE') and self.timer >= self.holdTime:
-		self.playAnim("idle")
+		self.playAnim(idles[Conductor.curBeat % idles.size()])
 
 func stepHit(_curStep):
 	self.timer += 1;
